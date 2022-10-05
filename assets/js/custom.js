e5afd0d3ab075e41e4e979018c65779453618da2
hmm365 <69030799+hmm365@users.noreply.github.com>
2022-10-05 09:28:00 +0900

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+// 로그인 버튼
+const loginBtn = document.querySelector(".loginBtn");
+const loginPopup = document.querySelector(".login__popup");
+const loginClose = document.querySelector(".btn-close");
+
+loginBtn.addEventListener("click", () => {
+  loginPopup.classList.add("open");
+});
+
+loginClose.addEventListener("click", () => {
+  loginPopup.classList.remove("open");
+});
